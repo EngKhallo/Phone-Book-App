33fd52751bf68168df9421253417a85a5f627503
Sir Khalid <engkhallo.ict@gmail.com>
2022-01-16 02:41:51 +0300

    Add functionality & clearing the input

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,32 +1,52 @@
 var Contacts = [];
-Contacts.push({ id: 1, name: "Khalid Abdi Guleid", phone: "0633544626" });
-Contacts.push({ id: 2, name: "Khalid Abdi Guleid", phone: "0633544626" });
-Contacts.push({ id: 3, name: "Khalid Abdi Guleid", phone: "0633544626" });
-for (var _i = 0, Contacts_1 = Contacts; _i < Contacts_1.length; _i++) {
-    var Contact = Contacts_1[_i];
+renderTable();
+function renderTable() {
     var tbodyElement = document.querySelector("tbody");
-    var row = document.createElement("tr");
-    tbodyElement.className = "text-lg";
-    row.className = "font-semibold";
-    var idColumn = document.createElement("td");
-    idColumn.textContent = Contact.id.toString();
-    idColumn.className = "text-center border-2 border-gray-500 py-2";
-    var nameColumn = document.createElement("td");
-    nameColumn.textContent = Contact.name;
-    nameColumn.className = "border-2 border-gray-500";
-    var phoneColumn = document.createElement("td");
-    phoneColumn.textContent = Contact.phone;
-    phoneColumn.className = "border-2 border-gray-500";
-    row.appendChild(idColumn);
-    row.appendChild(nameColumn);
-    row.appendChild(phoneColumn);
-    tbodyElement.appendChild(row);
+    tbodyElement.remove();
+    var newTbody = document.createElement('tbody');
+    newTbody.className = "text-lg";
+    for (var _i = 0, Contacts_1 = Contacts; _i < Contacts_1.length; _i++) {
+        var Contact = Contacts_1[_i];
+        var row = document.createElement("tr");
+        row.className = "font-semibold";
+        var idColumn = document.createElement("td");
+        idColumn.textContent = Contact.id.toString();
+        idColumn.className = "text-center border-2 border-gray-500 py-2";
+        var nameColumn = document.createElement("td");
+        nameColumn.textContent = Contact.name;
+        nameColumn.className = "border-2 border-gray-500";
+        var phoneColumn = document.createElement("td");
+        phoneColumn.textContent = Contact.phone;
+        phoneColumn.className = "border-2 border-gray-500";
+        row.appendChild(idColumn);
+        row.appendChild(nameColumn);
+        row.appendChild(phoneColumn);
+        newTbody.appendChild(row);
+    }
+    var table = document.querySelector('table');
+    table.appendChild(newTbody);
 }
 function add() {
-    var nameInput = document.querySelector('#name'); // this is cating; we converted the HTML Element to HTML Input element
+    var nameInput = document.querySelector("#name"); // this is cating; we converted the HTML Element to HTML Input element
     var name = nameInput.value;
-    var phoneInput = document.querySelector('#phone');
+    var phoneInput = document.querySelector("#phone");
     var number = phoneInput.value;
-    if (name.length < 3 && number.length < 7) {
+    if (name.length < 3 || number.length < 4) {
+        alert("Please provide a valid input value");
+        return;
     }
+    //   Todo: reject duplicate contacts
+    Contacts.push({
+        id: Contacts.length + 1,
+        name: name,
+        phone: number
+    });
+    renderTable();
+    clear();
+}
+function clear() {
+    var nameInput = document.querySelector("#name"); // this is cating; we converted the HTML Element to HTML Input element
+    nameInput.value = "";
+    var phoneInput = document.querySelector("#phone");
+    phoneInput.value = "";
 }
--- src/main.ts
+++ src/main.ts
@@ -6,45 +6,72 @@ type Contact = {
 };
 
 const Contacts: Contact[] = [];
-Contacts.push({ id: 1, name: "Khalid Abdi Guleid", phone: "0633544626" });
-Contacts.push({ id: 2, name: "Khalid Abdi Guleid", phone: "0633544626" });
-Contacts.push({ id: 3, name: "Khalid Abdi Guleid", phone: "0633544626" });
 
-for (const Contact of Contacts) {
-  const tbodyElement = document.querySelector("tbody");
-  const row = document.createElement("tr");
+renderTable();
 
-  tbodyElement.className = "text-lg";
-  row.className = "font-semibold";
 
-  const idColumn = document.createElement("td");
-  idColumn.textContent = Contact.id.toString();
-  idColumn.className = "text-center border-2 border-gray-500 py-2";
+function renderTable() {
+    const tbodyElement = document.querySelector("tbody");
+    tbodyElement.remove();
 
-  const nameColumn = document.createElement("td");
-  nameColumn.textContent = Contact.name;
-  nameColumn.className = "border-2 border-gray-500";
+    const newTbody = document.createElement('tbody');
+    newTbody.className = "text-lg";
+    
+  for (const Contact of Contacts) {
+    const row = document.createElement("tr");
 
-  const phoneColumn = document.createElement("td");
-  phoneColumn.textContent = Contact.phone;
-  phoneColumn.className = "border-2 border-gray-500";
+    row.className = "font-semibold";
 
-  row.appendChild(idColumn);
-  row.appendChild(nameColumn);
-  row.appendChild(phoneColumn);
+    const idColumn = document.createElement("td");
+    idColumn.textContent = Contact.id.toString();
+    idColumn.className = "text-center border-2 border-gray-500 py-2";
 
-  tbodyElement.appendChild(row);
+    const nameColumn = document.createElement("td");
+    nameColumn.textContent = Contact.name;
+    nameColumn.className = "border-2 border-gray-500";
+
+    const phoneColumn = document.createElement("td");
+    phoneColumn.textContent = Contact.phone;
+    phoneColumn.className = "border-2 border-gray-500";
+
+    row.appendChild(idColumn);
+    row.appendChild(nameColumn);
+    row.appendChild(phoneColumn);
+
+    newTbody.appendChild(row);
+  }
+
+  const table = document.querySelector('table');
+  table.appendChild(newTbody);
 }
 
+function add() {
+  const nameInput = document.querySelector("#name") as HTMLInputElement; // this is cating; we converted the HTML Element to HTML Input element
+  const name = nameInput.value;
 
-function add(){
-    const nameInput = document.querySelector('#name') as HTMLInputElement; // this is cating; we converted the HTML Element to HTML Input element
-    const name = nameInput.value;
+  const phoneInput = document.querySelector("#phone") as HTMLInputElement;
+  const number = phoneInput.value;
 
-    const phoneInput = document.querySelector('#phone') as HTMLInputElement;
-    const number = phoneInput.value;
+  if (name.length < 3 || number.length < 4) {
+    alert("Please provide a valid input value");
 
-    if(name.length<3 && number.length< 7){
-        
-    }
+    return;
+  }
+//   Todo: reject duplicate contacts
+
+  Contacts.push({
+      id: Contacts.length+1,
+      name: name,
+      phone: number,
+  });
+  renderTable();
+  clear();
+}
+function clear(){
+    const nameInput = document.querySelector("#name") as HTMLInputElement; // this is cating; we converted the HTML Element to HTML Input element
+    nameInput.value = "";
+    
+  
+    const phoneInput = document.querySelector("#phone") as HTMLInputElement;
+    phoneInput.value = "";
 }
